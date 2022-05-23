2db768fe476a476fe0a5e9dd98e5ea15a12bf277
hlista <hlista91@gmail.com>
2022-05-23 08:48:29 -0500

    Update vrising.sh

--- vrising.sh
+++ vrising.sh
@@ -1,25 +1,7 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
 sudo apt update && sudo apt upgrade -y 
 sudo apt install unzip apt-transport-https ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 sudo apt update
-
-#install AWS CLI
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
-sudo ./aws/install
-
-#create random string for password
-VHPW=$(echo $RANDOM | md5sum | head -c 20)
-
-#get stackname created by user data script and update SSM parameter name with this to make it unique
-STACKNAME=$(</tmp/mcParamName.txt)
-PARAMNAME=mcValheimPW-$STACKNAME
-
-#put random string into parameter store as encrypted string value
-aws ssm put-parameter --name $PARAMNAME --value $VHPW --type "SecureString" --overwrite
 
 #install docker
 sudo apt install docker-ce docker-ce-cli containerd.io -y
@@ -32,8 +14,12 @@ git clone https://github.com/Googlrr/V-Rising-Docker-Linux
 cd V-Rising-Docker-Linux
 sudo docker build . -t vrising:latest
 
-#install vrising app on docker
+#make serverconfig and saves and settings
 sudo mkdir /usr/games/serverconfig
+sudo mkdir /usr/games/serverconfig/vrising/saves
+sudo mv ./settings /usr/games/serverconfig/vrising/settings
+
+#install vrising app on docker
 cd /usr/games/serverconfig
 sudo bash -c 'echo "version: \"2.1\"
 services: 
@@ -48,5 +34,5 @@ services:
       - "27016:27016/udp"
     restart: unless-stopped
     tty: true" >> docker-compose.yml'
-echo "@reboot root (cd /usr/games/serverconfig/ && docker-compose up)" > /etc/cron.d/awsgameserver
+crontab -l | { cat; echo "@reboot root (cd /usr/games/serverconfig/ && docker-compose up)"; } | crontab -
 sudo docker-compose up
